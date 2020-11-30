0514079a6d9f59572bb57628822fabe8e522da35
= <=>
2020-11-30 06:04:25 +0100

    added score keeper

--- lib/main.dart
+++ lib/main.dart
@@ -7,38 +7,82 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return Named();
+  }
+}
+
+class Named extends StatefulWidget {
+  @override
+  _NamedState createState() => _NamedState();
+}
+
+class _NamedState extends State<Named> {
+  Row Answers() {}
+  List<Icon> scoreKeeper = [];
+
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.black,
         body: SafeArea(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Expanded(
-              flex: 4,
-              child: Text(
-                'What Question do you want to ask?',
-                style: TextStyle(color: Colors.white),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                flex: 8,
+                child: Center(
+                  child: Text(
+                    'What Question do you want to ask?',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
               ),
-            ),
-            Expanded(
-              flex: 1,
-              child: FlatButton(
-                  onPressed: () {},
-                  child: Text('True', style: TextStyle(color: Colors.white)),
-                  color: Colors.green),
-            ),
-            SizedBox(height: 30.0),
-            Expanded(
-              flex: 1,
-              child: FlatButton(
-                  color: Colors.red,
-                  onPressed: () {},
-                  child: Text('False', style: TextStyle(color: Colors.white))),
-            )
-          ],
-        )),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextButton(
+                    onPressed: () {
+                      setState(() {
+                        scoreKeeper.add(Icon(Icons.check, color: Colors.green));
+                      });
+                    },
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.green)),
+                    child: Text(
+                      'True',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextButton(
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.red)),
+                      onPressed: () {
+                        setState(() {
+                          scoreKeeper
+                              .add(Icon(Icons.cancel, color: Colors.red));
+                        });
+                      },
+                      child:
+                          Text('False', style: TextStyle(color: Colors.white))),
+                ),
+              ),
+              Row(
+                children: scoreKeeper,
+              )
+
+              //Todo: I want to code all day.
+            ],
+          ),
+        ),
       ),
     );
   }
